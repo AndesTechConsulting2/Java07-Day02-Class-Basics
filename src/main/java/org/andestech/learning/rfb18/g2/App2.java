b25b42dd92deffefea2b909046bab1f406817333
a0z9 <a0z9@rambler.ru>
2018-10-31 10:00:31 +0300

    comm1

---
+++ src/main/java/org/andestech/learning/rfb18/g2/App2.java
@@ -0,0 +1,30 @@
+package org.andestech.learning.rfb18.g2;
+
+
+import java.util.Random;
+
+public class App2
+{
+    public static void main( String[] args )  {
+
+
+       B.a = 1265765;
+       B b1 = new B();
+
+       b1.b = 29098098;
+        new B();
+
+        B.a = 10000;
+        new B();
+
+        B.test(1);
+        B.test("AAA");
+
+        B.test(1.2345);
+
+        B.test(1.7777, "iuyi");
+
+
+
+       }
+}
---
+++ src/main/java/org/andestech/learning/rfb18/g2/B.java
@@ -0,0 +1,44 @@
+package org.andestech.learning.rfb18.g2;
+
+public class B {
+
+    static
+    {
+
+        System.out.println("++ static ctor +++");
+        a = 837987;
+        MONTH = 10;
+    }
+
+    public B(){
+        System.out.println("++ ctor: " + this);
+    }
+
+    public static int a;
+    //public static final int MONTH = 10;
+    public static final int MONTH;
+
+    public int b;
+
+
+//    public static void test(int a)
+//    {
+//        System.out.println("test1: " +a );
+//    }
+
+    public static void test(String a)
+    {
+        System.out.println("test2: " +a );
+    }
+
+    public static void test(double a)
+    {
+        System.out.println("test3: " +a );
+    }
+
+    public static void test(double a, String b)
+    {
+        System.out.println("test4: " +a );
+    }
+
+}
